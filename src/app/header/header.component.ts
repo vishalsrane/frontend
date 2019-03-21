7270e4a4b78e4d92e4111ce45c72085e68961b24
bavithras <bavi.rane@gmail.com>
2019-03-21 09:57:01 +0530

    Added side navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isCollapsed: boolean = false;
+
+  toogleSideMenu(){
+    this.isCollapsed = !this.isCollapsed;
+    if(this.isCollapsed == true){
+      let sideMenuCol = document.getElementById('side-menu-toogle-button-col');
+      sideMenuCol.className = 'col-sm-1 side-menu-toogle-button-col-small';
+      let sideNavigationCol = document.getElementById('side-navigation-col');
+      sideNavigationCol.className = 'side-navigation-none';
+      let mainContentCol = document.getElementById('main-content-col');
+      mainContentCol.className = "col-md-12";
+    }else{
+      let sideMenuCol = document.getElementById('side-menu-toogle-button-col');
+      sideMenuCol.className = 'col-sm-2 side-menu-toogle-button-col';
+      let sideNavigationCol = document.getElementById('side-navigation-col');
+      sideNavigationCol.className = 'col-md-2 side-navigation-col';
+      let mainContentCol = document.getElementById('main-content-col');
+      mainContentCol.className = "col-md-10";
+    }
+  }
+
   constructor() { }
 
   ngOnInit() {
